2f8f18eb0963735c262a1ee071e82ae981db806d
Zucky <usupfv@gmail.com>
2025-03-08 16:30:14 +0900

    add mock api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  /**
+   * FIXME:Remove this function
+   */
+  const search = async (): Promise<void> => {
+    fetch("http://localhost:8080/api/books?q=example")
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error:", error));
+  };
+
+  search();
 
   return (
     <>
@@ -29,7 +41,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
