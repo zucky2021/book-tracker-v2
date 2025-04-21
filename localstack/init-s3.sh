e1b996376f93b88b0e5abb9b4a17c65c93c653f1
Zucky <usupfv@gmail.com>
2025-04-22 07:12:08 +0900

    8 Add ini s3 sh err handle

--- localstack/init-s3.sh
+++ localstack/init-s3.sh
@@ -2,6 +2,9 @@
 
 # This script is used to initialize the S3 bucket in LocalStack
 
+# Exit if an error occurs
+set -e
+
 echo "=== Initializing S3 bucket in LocalStack ==="
 
 # Make bucket
@@ -12,9 +15,15 @@ awslocal s3 cp --recursive /init-data/ s3://local-book-tracker/
 
 # Check
 echo "Checking S3 bucket..."
-awslocal s3 ls
+awslocal s3 ls || {
+  echo "Failed to list buckets"
+  exit 1
+}
 
 echo "Checking S3 bucket contents..."
-awslocal s3 ls --recursive s3://local-book-tracker/
+awslocal s3 ls --recursive s3://local-book-tracker/ || {
+  echo "Failed to list bucket contents"
+  exit 1
+}
 
 echo "=== Completed initializing S3 bucket in LocalStack ==="
