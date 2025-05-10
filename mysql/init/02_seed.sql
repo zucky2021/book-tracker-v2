e1bfdfeed771994961f9022c1285ccf3b36a86ee
Zucky <usupfv@gmail.com>
2025-05-10 09:32:39 +0900

    22 Fix seed.sql

--- mysql/init/02_seed.sql
+++ mysql/init/02_seed.sql
@@ -13,14 +13,14 @@ VALUES
     'iniBook1',
     'Initial test memo 1',
     '20250101120000123456789.jpg',
-    NOW (),
-    NOW ()
+    NOW(),
+    NOW()
   ),
   (
     'iniUser1',
     'iniBook2',
     'Initial test memo 2',
     '20250102130000123456789.png',
-    NOW (),
-    NOW ()
+    NOW(),
+    NOW()
   );
